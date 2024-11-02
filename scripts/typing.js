19dd06351a448a922d40739a308ff588e5c24bde
LiquidRetarded <tuilazerotwo@gmail.com>
2024-11-02 17:44:40 +0700

    a

---
+++ scripts/typing.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#typed1', {
+    strings: ['Tình yêu của đôi ta (Nkhoi và Cly) đã được: '],
+    typeSpeed: 50,
+    backSpeed: 0, 
+    loop: false,
+    direction: 'rtl' 
+});
+var typed = new Typed('#typed2', {
+    strings: ['Tính từ ngày 10/6/2024 :3'],
+    typeSpeed: 100,
+    backSpeed: 0, 
+    loop: false,
+    direction: 'rtl' 
+});
